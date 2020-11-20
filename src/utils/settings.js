67916729dc96cc027a15174bf5f98082156fb0d4
Jarne <jarne@users.noreply.github.com>
2020-11-20 13:04:07 +0100

    Added build badge, fixed to production URL

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -4,5 +4,5 @@
 
 export const settingsValues = {
     spotify_client_id: "f2b889eac3784a478badb5316bce96bc",
-    encoded_redirect_uri: "http%3A%2F%2Flocalhost%3A3000",
+    encoded_redirect_uri: "https%3A%2F%2Fgoodvibesfilter.layst.rocks",
 }
