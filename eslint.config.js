cb042e3a06df40a6bae751b0aa78ecd14b859848
Jarne <dev@jarne.rocks>
2025-04-13 20:31:50 +0200

    Rewrite ESLint config in new version with JS and React

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from "eslint/config"
+import js from "@eslint/js"
 import globals from "globals"
 import react from "eslint-plugin-react"
 
+const allJsTsFiles = "**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"
+
 export default defineConfig([
     {
+        files: [allJsTsFiles],
+        ...js.configs.recommended,
+    },
+    {
+        files: [allJsTsFiles],
         ...react.configs.flat.recommended,
         languageOptions: {
             ...react.configs.flat.recommended.languageOptions,
@@ -11,8 +19,16 @@ export default defineConfig([
                 ...globals.browser,
             },
         },
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
         rules: {
-            "react-hooks/exhaustive-deps": "off",
+            ...react.configs.flat.recommended.rules,
+            ...react.configs.flat["jsx-runtime"].rules,
+            "react/prop-types": "off",
+            "react/no-unescaped-entities": "off",
         },
     },
 ])
